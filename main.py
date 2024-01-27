f6cf418a6499360f6814be7010046372fa8d9015
Tarek Lein <91937397+TrakeLean@users.noreply.github.com>
2024-01-27 15:02:15 +0100

    asd

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import time
+from flask import Flask, request
 
-print("Hello @@ ", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-print("GitHub")
+app = Flask(__name__)
+
+@app.route('/webhook-endpoint', methods=['POST'])
+def webhook():
+    # Your webhook logic goes here
+    print("Received a webhook event!")
+
+    # Access the request payload
+    payload = request.get_data(as_text=True)
+    print("Webhook payload:", payload)
+
+    return '', 200  # Respond to GitHub with a 200 OK status
+
+if __name__ == '__main__':
+    app.run(port=8080, host='0.0.0.0')  # Listen on all public IPs
