c2c13bfb2dad23a05279e9e7457d4f84b63cb9cf
Tarek School <Tarek_rl99@hotmail.com>
2024-01-29 20:54:11 +0000

    Everything set up for automatic GitHub pulls

--- git-config/git-pre.sh
+++ git-config/git-pre.sh
@@ -71,3 +71,40 @@ fi
 # parent_folder_name=$(echo "$parent_folder_name" | tr '[:upper:]' '[:lower:]')
 
 # echo "Parent folder name: $parent_folder_name"
+
+
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    # Ask if the user wants to install Docker
+    read -p ">🐳 Docker is not installed. Do you want to install Docker? (y/n): " InstallDocker
+
+    if [ "$InstallDocker" = "y" ]; then
+        # Inform the user about the installation process
+        echo -n ">⌛ Installing Docker..."
+
+        # Check the Linux distribution and install Docker accordingly
+        if [ -f /etc/redhat-release ]; then
+            # For Red Hat-based systems (e.g., CentOS)
+            sudo yum install -y docker
+        elif [ -f /etc/debian_version ]; then
+            # For Debian-based systems (e.g., Ubuntu)
+            sudo apt-get update
+            sudo apt-get install -y docker.io
+        else
+            # Unsupported Linux distribution
+            echo -e "\r\033[K>❌ Unsupported Linux distribution. Please install Docker manually."
+            exit 1
+        fi
+
+        # Start Docker service
+        sudo systemctl start docker
+
+        # Display success message
+        echo -e "\r\033[K>✅ Docker has been installed successfully."
+    else
+        # User chose not to install Docker
+        echo ">🐳 Docker is required for this script. Exiting."
+        exit 1
+    fi
+fi
--- test.sh
+++ test.sh
@@ -6,27 +6,51 @@ if [ ! -d ".git" ]; then
     echo ">❌ There is no .git folder in this directory. Exiting."
     exit 1
 fi
-echo -e "\r\033[K>✅ Checking if .git folder exists..."
+echo -e "\r\033[K>✅ Checking if .git folder exists...  Successful"
 
 # Extract remote URL and repository name
 RemoteUrl=$(git config --get remote.origin.url)
-RepoName=$(basename -s .git $RemoteUrl)
+RepoName=$(basename -s .git $RemoteUrl | tr '[:upper:]' '[:lower:]')
 
-# Setup variables for GitHub Actions
-WorkflowDir="./Automation"
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    # Ask if the user wants to install Docker
+    read -p ">🐳 Docker is not installed. Do you want to install Docker? (y/n): " InstallDocker
 
-# Setup variables for configuration file
-ConfigurationFilePath="$WorkflowDir/settings.json"
-ScriptPath="./main.py"
+    if [ "$InstallDocker" = "y" ]; then
+        # Inform the user about the installation process
+        echo -n ">⌛ Installing Docker..."
+
+        # Check the Linux distribution and install Docker accordingly
+        if [ -f /etc/redhat-release ]; then
+            # For Red Hat-based systems (e.g., CentOS)
+            sudo yum install -y docker
+        elif [ -f /etc/debian_version ]; then
+            # For Debian-based systems (e.g., Ubuntu)
+            sudo apt-get update
+            sudo apt-get install -y docker.io
+        else
+            # Unsupported Linux distribution
+            echo -e "\r\033[K>❌ Unsupported Linux distribution. Please install Docker manually."
+            exit 1
+        fi
+
+        # Start Docker service
+        sudo systemctl start docker
+
+        # Display success message
+        echo -e "\r\033[K>✅ Docker has been installed successfully."
+    else
+        # User chose not to install Docker
+        echo ">🐳 Docker is required for this script. Exiting."
+        exit 1
+    fi
+fi
 
 # Setup variables for Docker file
-DockerFilePath="$WorkflowDir/Dockerfile"
+DockerFilePath="./Dockerfile"
 
-# Create the directory if it doesn't exist
-echo -n ">⌛ Setting up directory..."
-mkdir -p "$WorkflowDir"
-echo -e "\r\033[K>✅ Setting up directory..."
-
+# Check if Dockerfile already exists if not create it
 if [ ! -f $DockerFilePath ]; then
     echo -n ">⌛ Creating Dockerfile..."  
     cat <<EOL > "$DockerFilePath"
@@ -42,28 +66,55 @@ COPY ../main.py .
 # Define the command to run on container start
 CMD ["python", "main.py"]
 EOL
+  echo -e "\r\033[K>✅ Creating Dockerfile...   Successful"
+else
+  echo -e "\r\033[K>✅ Dockerfile already exists... Skipping"
 fi
-echo -e "\r\033[K>✅ Creating Dockerfile..."
 
 # Build and run the Docker image
-echo -n ">⌛ Building Docker image \"$RepoName\"..."
-docker build -t $RepoName .
-echo -e "\r\033[K>✅ Building Docker image \"$RepoName\"..."
-docker run -it -d --name $RepoName $RepoName
-echo -n ">✅ Running Docker image \"$RepoName\"..."
+echo -n -e ">⌛ Building Docker image \"$RepoName\"...\n"
+sudo docker build -t $RepoName .
+build_status=$?
+if [ $build_status -eq 0 ]; then
+    echo -e "\r\033[K>✅ Building Docker image \"$RepoName\"... Successful"
+    echo -n -e "\n>⌛ Running Docker image \"$RepoName\"..."
+    sudo docker run -it -d --name $RepoName $RepoName
+    run_status=$?
+    if [ $run_status -eq 0 ]; then
+        echo -n "\r\033[K>✅ Running Docker image \"$RepoName\"... Successful"
+    else
+        echo -e "\r\033[K>❌ Running Docker image \"$RepoName\"... Failed"
+        exit 1
+    fi
+else
+    echo -e "\r\033[K>❌ Building Docker image \"$RepoName\"... Failed"
+    exit 1
+fi
 
 # Add the workflow file to Git
 echo -n ">⌛ Adding all files to Git..."
 git add .
-echo  -e "\r\033[K>✅ Adding all files to Git..."
+add_status=$?
+echo -e "\r\033[K>✅ Adding all files to Git... Successful"
 
 # Commit the changes
 echo -n ">⌛ Committing the changes..."
-git commit -m "Everthing setup for automatic GitHub pulls"
-echo -e "\r\033[K>✅ Committing the changes..."
+git commit -m "Everything set up for automatic GitHub pulls"
+commit_status=$?
+if [ $commit_status -eq 0 ]; then
+    echo -e "\r\033[K>✅ Committing the changes... Successful"
+else
+    echo -e "\r\033[K>❌ Committing the changes... Failed"
+    exit 1
+fi
 
 # Push the changes
 echo -n ">⌛ Pushing the changes..."
 git push origin main
-echo -e "\r\033[K>✅ Pushing the changes..."
+push_status=$?
+if [ $push_status -eq 0 ]; then
+    echo -e "\r\033[K>✅ Pushing the changes... Successful"
+else
+    echo -e "\r\033[K>❌ Pushing the changes... Failed"
+    exit 1
 fi
