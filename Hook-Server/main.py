655399d3b011bd25f1bec33f43a8e2aa06436acd
Tarek School <Tarek_rl99@hotmail.com>
2024-01-29 23:46:53 +0000

    :)

--- Hook-Server/main.py
+++ Hook-Server/main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route('/git-webhook', methods=['POST'])
 def webhook():
     try:
+        print("Webhook received")
         payload = request.get_data(as_text=True)
         data = json.loads(payload)
 
