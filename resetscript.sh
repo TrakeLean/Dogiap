edfeba4ab5efd048e39422b1fb649dd628b11b14
Tarek Mac <Tarek_rl99@hotmail.com>
2024-02-01 01:05:48 +0000

    :)

--- resetscript.sh
+++ resetscript.sh
@@ -1,11 +1,14 @@
-# Remove the .deb file
-sudo dpkg -i ~/Dogiap.deb
+# # Remove the .deb file
+# sudo dpkg -i ~/Dogiap.deb
 
-# apt remove dogiap
-sudo apt-get remove -y dogiap
+# # apt remove dogiap
+# sudo apt-get remove -y dogiap
 
-# Build the Debian package
-dpkg-deb --build Dogiap
+# # Build the Debian package
+# dpkg-deb --build Dogiap
 
-# Install the Debian package
-sudo dpkg -i ~/Dogiap.deb
+# # Install the Debian package
+# sudo dpkg -i ~/Dogiap.deb
+
+CurrentRemoteUrl=$(git remote get-url origin) > /dev/null 2>&1
+echo "Current remote url: $CurrentRemoteUrl"
