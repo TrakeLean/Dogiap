e5d90e1cdaa293dce218dee4620654f093115eba
Tarek Mac <Tarek_rl99@hotmail.com>
2024-02-06 02:53:11 +0000

    Might be fully working

--- Hook-Server/main.py
+++ Hook-Server/main.py
@@ -37,39 +37,19 @@ def webhook():
             error_raise(ContainerName)
             error_raise(ProgramPath)
             
-            stop_cmd = f'docker stop {ContainerName}'
-            delete_cmd = f'docker rm {ContainerName}'
             pull_cmd = f'cd {ProgramPath} && git pull'
-            build_cmd = f'cd {ProgramPath} && docker build -t {ContainerName} .'
-            run_cmd = f'docker run -it -d --name {ContainerName} {ContainerName}'
-
-            # Stop the container
-            stop_result = subprocess.run(stop_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            if stop_result.returncode != 0:
-                logger.error(f"Error stopping container: {stop_result.stderr}")
+            restart_cmd = f'sudo systemctl restart dogiap-{ContainerName}'
                 
-            # Delete the container
-            delete_result = subprocess.run(delete_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            if delete_result.returncode != 0:
-                logger.error(f"Error deleting container: {delete_result.stderr}")
-
             # Pull changes from the Git repository
             pull_result = subprocess.run(pull_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             if pull_result.returncode != 0:
                 logger.error(f"Error pulling changes: {pull_result.stderr}")
                 return jsonify({"error": "Internal Server Error"}), 500
 
-            # Build the Docker image
-            build_result = subprocess.run(build_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            if build_result.returncode != 0:
-                logger.error(f"Error building Docker image: {build_result.stderr}")
-                return jsonify({"error": "Internal Server Error"}), 500
-
-            # Start the container
-            start_result = subprocess.run(run_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            if start_result.returncode != 0:
-                logger.error(f"Error starting container: {start_result.stderr}")
-                return jsonify({"error": "Internal Server Error"}), 500
+            # Delete the container
+            delete_result = subprocess.run(restart_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            if delete_result.returncode != 0:
+                logger.error(f"Error deleting container: {delete_result.stderr}")
 
             logger.info(f'GitHub - Push event: {ContainerName} updated and restarted')
             return jsonify({"message": f'GitHub - Push event: {ContainerName} updated and restarted'}), 200
